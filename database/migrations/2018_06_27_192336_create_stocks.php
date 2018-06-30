d2c2ea3a967d0581841d520ba6348c6ed23c3646
Kazuya Mashiro <kazuyafaizad@gmail.com>
2018-07-01 03:54:54 +0800

    Commit Stock

--- database/migrations/2018_06_27_192336_create_stocks.php
+++ database/migrations/2018_06_27_192336_create_stocks.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateStockIns extends Migration
+class CreateStocks extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,8 @@ class CreateStockIns extends Migration
      */
     public function up()
     {
-        Schema::create('stock_ins', function (Blueprint $table) {
+        Schema::dropIfExists('stocks');
+        Schema::create('stocks', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('supplier_id');
             $table->date('stock_date');
